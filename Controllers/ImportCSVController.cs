2b43efeba15552ce8a49e1e01f13fefd647d6ba0
Artem Anpilogov <artem.anpilogov>
2023-09-24 08:13:34 +0300

    importcsv

--- Controllers/ImportCSVController.cs
+++ Controllers/ImportCSVController.cs
@@ -1,45 +1,23 @@
-using System.Text;
 using System.Xml;
-using System.Xml.Linq;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
 public class ImportCSVController: ControllerBase
 {
     [HttpPost]
-    [Route("api/importcsv")]    
+    [Route("api/ImportXmlToCsv")]    
     [Consumes("application/xml")]
-    [Produces("application/xml")]
-    public async Task<IActionResult> Importcsv([FromBody] FileContent fileContent)
-    { 
-        
-        // if (fileContent.FileXML != null)
-        // {
-        //     //XmlDocument doc = new XmlDocument();
-        //     //MemoryStream ms = new MemoryStream(fileContent.FileXML);
-        //     //doc.Load(ms);
-        //     return "ok";
-        // }
-
-        await Task.CompletedTask;
-        return Ok(fileContent); // just echo out for now to show the output serialization
-    }
-
-    [HttpPost]
-    [Route("api/importcsv2")]    
-    [Consumes("application/json")]
-    [Produces("application/json")]
-    public string Importcsv2(string text1)
-    { 
-        
-        // if (fileContent.FileXML != null)
-        // {
-        //     //XmlDocument doc = new XmlDocument();
-        //     //MemoryStream ms = new MemoryStream(fileContent.FileXML);
-        //     //doc.Load(ms);
-        //     return "ok";
-        // }
-        
-        return text1;
+    public List<string> ImportXmlToCsv([FromBody] XliffFile xliffFile)
+    {
+        List<string> csv = new List<string>();
+        if (xliffFile != null)
+        {   
+            foreach(var item in xliffFile.file.body.group.transUnit)
+            {                              
+                csv.Add(string.Join(";", item.source + ':' + item.target));
+            }
+            return csv;
+        }
+        return csv;
     }
 }
--- FileContent.cs
+++ FileContent.cs
@@ -1,5 +1,35 @@
-public class FileContent
+using System.Xml.Serialization;
+
+[XmlRoot(ElementName = "xliff", Namespace = "urn:oasis:names:tc:xliff:document:1.2")]
+public class XliffFile
 {
-    public string? Source { get; set; } 
-    public string? Target { get; set; }
+    [XmlElement(ElementName = "file")]
+    public File file {get; set;}
+}
+
+public class File
+{
+    [XmlElement(ElementName = "body")]
+    public Body body {get; set;}
+}
+
+public class Body
+{
+    [XmlElement(ElementName = "group")]
+    public Group group {get; set;}
+}
+
+public class Group
+{
+    [XmlElement(ElementName = "trans-unit")]
+    public List<TransUnit> transUnit {get; set;}
+}
+
+public class TransUnit
+{
+    [XmlElement(ElementName = "source")]
+    public string? source { get; set; }
+
+    [XmlElement(ElementName = "target")]
+    public string? target { get; set; }
 }
