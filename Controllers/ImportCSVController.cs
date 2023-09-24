cb9883a83fe8b83d7379c3c09956ec5e2a980953
Artem Anpilogov <artem.anpilogov>
2023-09-24 23:38:44 +0300

    export xml to csv

--- Controllers/ImportCSVController.cs
+++ Controllers/ImportCSVController.cs
@@ -9,18 +9,18 @@ public class ImportCSVController: ControllerBase
     [Route("api/ImportXmlToCsv")]    
     [Consumes("application/xml")]
     [Produces("text/csv")]
-    public async Task<FileResult> ImportXmlToCsv([FromBody] XliffFile xliffFile)
+    public FileResult ImportXmlToCsv([FromBody] XliffFile xliffFile)
     {
-        StringBuilder listCsv = new StringBuilder();
+        StringBuilder listCSV = new StringBuilder();
         if (xliffFile != null)
         {   
             foreach(var item in xliffFile.file.body.group.transUnit)
             {    
-                listCsv.AppendLine(item.source + ',' + item.target + ";");                          
+                listCSV.AppendLine(string.Format("\"{0}\",\"{1}\"", item.source, item.target));
             }        
         }
         
-        byte[] fileBytes  = Encoding.ASCII.GetBytes(listCsv.ToString());
-        return File(fileBytes, "text/csv", "text.csv");
+        byte[] fileBytes  = Encoding.ASCII.GetBytes(listCSV.ToString());
+        return File(fileBytes, "text/csv");
     }
 }
