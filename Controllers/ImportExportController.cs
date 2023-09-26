80a6630c5d72b3061ad808edde5ba798f45926aa
Artem Anpilogov <artem.anpilogov>
2023-09-26 13:49:04 +0300

    csv to xml

--- Controllers/ImportExportController.cs
+++ Controllers/ImportExportController.cs
@@ -1,5 +1,9 @@
+using System.Globalization;
 using System.Text;
 using System.Xml;
+using System.Xml.Serialization;
+using CsvHelper;
+using CsvHelper.Configuration;
 using Microsoft.AspNetCore.Mvc;
 
 [ApiController]
@@ -9,7 +13,7 @@ public class ImportExportController: ControllerBase
     [Route("api/ExportXmlToCsv")]
     [Consumes("application/xml")]
     [Produces("text/csv")]
-    public FileResult ExportXmlToCsv([FromBody] XmlFile xmlFile)
+    public FileResult ExportXmlToCsv(XmlFile xmlFile)
     {
         StringBuilder listCsv = new StringBuilder();
         if (xmlFile != null)
@@ -26,10 +30,46 @@ public class ImportExportController: ControllerBase
 
     [HttpPost]
     [Route("api/ImportCsvToXml")]
-    [Consumes("application/xml")]
-    [Produces("text/csv")]
-    public void ImportCsvToXml()
+    public async Task<IActionResult> ImportCsvToXml()
     {        
+        try
+        {
+            //using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            using (StreamReader reader = new StreamReader("D:\\test.txt", Encoding.UTF8))
+            {
+                string stringFiles = await reader.ReadToEndAsync();
+                string[] file = stringFiles.Split("artendstop");
 
+                List<CsvFile> csvFile = StringToCsv(file[0]);
+                XmlFile xmlFile = StringToXml(file[1]);
+
+                return base.Ok();
+            }
+        }
+        catch(Exception ex)
+        {
+            return base.Problem(ex.Message);
+        }
+    }
+
+    private XmlFile StringToXml(string file)
+    {
+        using(TextReader sReader = new StringReader(file))
+        {
+            var serializer = new XmlSerializer(typeof(XmlFile));
+            XmlFile xmlFile =  (XmlFile)serializer.Deserialize(sReader);            
+            return xmlFile;
+        }        
+    }
+
+    private List<CsvFile> StringToCsv(string file)
+    {
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = false };
+        using (var sReader = new StringReader(file))
+        using (var csvReader = new CsvReader(sReader, config))
+        {                    
+            List<CsvFile> csvFile = csvReader.GetRecords<CsvFile>().ToList();
+            return csvFile;
+        }        
     }
 }
---
+++ CsvFile.cs
@@ -0,0 +1,10 @@
+using CsvHelper.Configuration.Attributes;
+
+public class CsvFile
+{
+    [Index(0)]
+    public string? Source {get; set;}
+
+    [Index(1)]
+    public string? Target {get; set;}
+}
